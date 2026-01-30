a62ebc48745a331ae2c0364beccf96ee04ea44f9
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-01-30 10:35:02 +0100

    add the method index for getting the users

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,8 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+
+    public function index(Request $request){
+        $users = User::where('name', 'like', "%{$request->q}%")
+        ->orWhere('specialite', 'like', "%{$request->q}%")
+        ->get();
+        
+
+        return view('dashboard', compact($users));
+    }
 }
