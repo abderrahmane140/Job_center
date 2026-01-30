e0f48b1290a2ed2aa9bc834d1f7217987af379b2
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-01-30 12:22:40 +0100

    fix: pass a string to func compact insted of a variable

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,10 +10,10 @@ class SearchController extends Controller
 
     public function index(Request $request){
         $users = User::where('name', 'like', "%{$request->q}%")
-        ->orWhere('specialite', 'like', "%{$request->q}%")
+        ->orWhere('specialty', 'like', "%{$request->q}%")
         ->get();
         
 
-        return view('dashboard', compact($users));
+        return view('dashboard', compact('users'));
     }
 }
