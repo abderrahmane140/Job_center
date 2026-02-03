f9d47ab56081873b38c37c9d5ce3893f3a58bdfb
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-03 14:48:29 +0100

    update the name of migration from jobs to job_offers

--- database/migrations/2026_02_03_112201_create_jobs_table.php
+++ database/migrations/2026_02_03_112201_create_jobs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('job_offers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
