b2083bc40c953e2402a521abf75cab1aa14c639b
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-04 12:13:25 +0100

    update the seeder for the role add spatie permission

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 
 class RoleSeeder extends Seeder
 {
@@ -13,7 +15,35 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'recruiter']);    
-        Role::create(['name' => 'job_seeker']);
+        $recruiter = Role::create(['name' => 'recruiter']);    
+        $jobSeeker = Role::create(['name' => 'job_seeker']);
+
+        //Permissions
+        $permissions = [
+            'create job offer',
+            'edit job offer',
+            'close job offer',
+            'apply job',
+            'view applications',
+            'edit cv',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+
+         // Assign permissions
+        $recruiter->givePermissionTo([
+            'create job offer',
+            'edit job offer',
+            'close job offer',
+            'view applications',
+        ]);
+
+        $jobSeeker->givePermissionTo([
+            'apply job',
+            'edit cv',
+        ]);
     }
 }
