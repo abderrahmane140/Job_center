8222a80a65e8356fb817f6281b1e29b38de8ebc7
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-05 17:05:26 +0100

    remove the spatie code form the roleseeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,35 +15,6 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $recruiter = Role::create(['name' => 'recruiter']);    
-        $jobSeeker = Role::create(['name' => 'job_seeker']);
-
-        //Permissions
-        $permissions = [
-            'create job offer',
-            'edit job offer',
-            'close job offer',
-            'apply job',
-            'view applications',
-            'edit cv',
-        ];
-
-        foreach ($permissions as $permission) {
-            Permission::firstOrCreate(['name' => $permission]);
-        }
-
-
-         // Assign permissions
-        $recruiter->givePermissionTo([
-            'create job offer',
-            'edit job offer',
-            'close job offer',
-            'view applications',
-        ]);
-
-        $jobSeeker->givePermissionTo([
-            'apply job',
-            'edit cv',
-        ]);
+       
     }
 }
