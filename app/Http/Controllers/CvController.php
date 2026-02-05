d528c5a040562d4b719394d6c3d89c497fcbd454
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-05 10:41:08 +0100

    create the cv controller

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Cv;
 
 class CvController extends Controller
 {
-    public function index(){
-        $cv = auth()->user()->cv;
+    public function index()
+    {
+        $user = Auth::user();
 
-        return view('cv.index', compact($cv));
+        $cv = $user->cv()->firstOrCreate(
+            ['user_id' => $user->id],
+            ['title' => 'My CV']
+        );
+
+        return view('cv.index', [
+            'experiences' => $cv->experiences,
+            'educations'  => $cv->educations,
+            'skills'      => $cv->skills,
+        ]);
     }
+
 
 
     public function create() {
