e34d3037ac3c0179fca6cdfccac9b6d76b3514e8
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-05 17:07:23 +0100

    apply the middleware recruiter to all the routes

--- routes/web.php
+++ routes/web.php
@@ -26,51 +26,34 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth'])->group(function() {
-    // Recruiter routes
-    Route::get('/job-offers', [JobOfferController::class, 'index'])
-         ->name('job_offers.index');
-
-    Route::get('/job-offers/create', [JobOfferController::class, 'create'])
-         ->name('job_offers.create');
-    Route::post('/job-offers', [JobOfferController::class, 'store'])
-         ->name('job_offers.store');
-    Route::delete('/job-offers/{jobOffer}', [JobOfferController::class, 'destroy'])
-         ->name('job_offers.destroy');
-
-    // Job Seeker route
-    Route::get('/job-offers/list', function () {
-        return view('job_offers.list');
-    })->name('job_offers.list');
+// RECRUITER ROUTES - Fixed middleware name
+Route::middleware(['auth', 'role:recruiter'])->group(function () {
+    Route::get('/job-offers', [JobOfferController::class, 'index'])->name('job_offers.index');
+    Route::get('/job-offers/create', [JobOfferController::class, 'create'])->name('job_offers.create');
+    Route::post('/job-offers', [JobOfferController::class, 'store'])->name('job_offers.store');
+    Route::delete('/job-offers/{jobOffer}', [JobOfferController::class, 'destroy'])->name('job_offers.destroy');
 });
 
-
-
-
+// JOB SEEKER ROUTES - Fixed middleware name
 Route::middleware(['auth', 'role:job_seeker'])->group(function () {
-    Route::get('/job-offers', function () {
+    Route::get('/browse-jobs', function () {
         return view('job_offers.list');
     })->name('job_offers.list');
+
+    Route::post('/job-offers/{jobOffer}/apply', [ApplicationController::class, 'store'])
+        ->name('job-offers.apply');
+
+    // CV Routes
+    Route::get('/cv', [CvController::class, 'index'])->name('cv.index');
+    
+    Route::post('/experience', [ExperienceController::class, 'store'])->name('experiences.store');
+    Route::delete('/experience/{experience}', [ExperienceController::class, 'destroy'])->name('experiences.destroy');
+
+    Route::post('/education', [EducationController::class, 'store'])->name('educations.store');
+    Route::delete('/education/{education}', [EducationController::class, 'destroy'])->name('educations.destroy');
+
+    Route::post('/skills', [SkillController::class, 'store'])->name('skills.store');
+    Route::delete('/skills/{skill}', [SkillController::class, 'destroy'])->name('skills.destroy');
 });
-
-Route::get('offers', [JobOfferController::class, 'index'])->name('offers');
-
-Route::middleware(['role:job_seeker'])->group(function () {
-    Route::post('/job-offers/{jobOffer}/apply', [ApplicationController::class, 'store']);
-});
-
-
-Route::get('/cv' , [CvController::class, 'index'])->name('cv.index');
-
-Route::post('/experience' , [ExperienceController::class , 'store'])->name('experiences.store');
-Route::delete('/experience/{experience}' , [ExperienceController::class , 'destroy'])->name('experiences.destroy');
-
-Route::post('/education' , [EducationController::class , 'store'])->name('educations.store');
-Route::delete('/education/{education}' , [EducationController::class , 'destroy'])->name('educations.destroy');
-
-Route::post('/skills' , [SkillController::class , 'store'])->name('skills.store');
-Route::delete('/skills/{skill}' , [SkillController::class , 'destroy'])->name('skills.destroy');
-
 
 require __DIR__.'/auth.php';
-
