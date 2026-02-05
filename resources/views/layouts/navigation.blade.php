c06adfd8002fba39fc9dddd32a3b412d3747fde3
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-05 17:06:24 +0100

    add the route for the offers and cv on the nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,15 +16,21 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
 
-                    <!-- New CV link -->
-                    <x-nav-link :href="route('cv.index')" :active="request()->routeIs('cv')">
-                        {{ __('My CV') }}
-                    </x-nav-link>
+                    @if(auth()->check() && auth()->user()->role === 'recruiter')
+                        <x-nav-link :href="route('job_offers.index')" :active="request()->routeIs('job_offers.index')">
+                            {{ __('Manage Job Offers') }}
+                        </x-nav-link>
+                    @endif
 
-                    <!-- New Offers link -->
-                    <x-nav-link :href="route('offers')" :active="request()->routeIs('offers')">
-                        {{ __('Job Offers') }}
-                    </x-nav-link>
+                    @if(auth()->check() && auth()->user()->role === 'job_seeker')
+                        <x-nav-link :href="route('job_offers.list')" :active="request()->routeIs('job_offers.list')">
+                            {{ __('Browse Jobs') }}
+                        </x-nav-link>
+
+                        <x-nav-link :href="route('cv.index')" :active="request()->routeIs('cv.index')">
+                            {{ __('My CV') }}
+                        </x-nav-link>
+                    @endif
                 </div>
             </div>
 
@@ -76,22 +82,27 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1 text-white">
+        <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
 
-            <!-- New CV link -->
-            <x-responsive-nav-link :href="route('cv.index')" :active="request()->routeIs('cv.index')">
-                {{ __('My CV') }}
-            </x-responsive-nav-link>
+            @if(auth()->check() && auth()->user()->role === 'recruiter')
+                <x-responsive-nav-link :href="route('job_offers.index')" :active="request()->routeIs('job_offers.index')">
+                    {{ __('Manage Job Offers') }}
+                </x-responsive-nav-link>
+            @endif
 
-            <!-- New Offers link -->
-            <x-responsive-nav-link :href="route('offers')" :active="request()->routeIs('offers')">
-                {{ __('Job Offers') }}
-            </x-responsive-nav-link>
+            @if(auth()->check() && auth()->user()->role === 'job_seeker')
+                <x-responsive-nav-link :href="route('job_offers.list')" :active="request()->routeIs('job_offers.list')">
+                    {{ __('Browse Jobs') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('cv.index')" :active="request()->routeIs('cv.index')">
+                    {{ __('My CV') }}
+                </x-responsive-nav-link>
+            @endif
         </div>
-
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
