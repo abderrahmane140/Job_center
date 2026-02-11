aa6278762df07d5d665119e592ec9cf896d7a054
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-11 02:32:05 +0100

    update the search controller to implement the status of the friendship

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,20 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Friendship;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
 
-    public function index(Request $request)
-    {
-        $query = $request->input('profile');
-
-        $users = User::where('name', 'like', "%{$query}%")
-            ->orWhere('specialty', 'like', "%{$query}%")
+public function index(Request $request)
+{
+    $query = $request->input('profile');
+    
+    $users = collect();
+    
+    if ($query) {
+        $users = User::where('id', '!=', auth()->id()) 
+            ->where(function($q) use ($query) {
+                $q->where('name', 'like', "%{$query}%")
+                  ->orWhere('specialty', 'like', "%{$query}%")
+                  ->orWhere('bio', 'like', "%{$query}%");
+            })
             ->get();
 
-        return view('dashboard', compact('users'));
+        $users = $users->map(function($user) {
+            // Check if there's a pending or accepted friendship
+            $friendship = Friendship::where(function($q) use ($user) {
+                $q->where('sender_id', auth()->id())
+                  ->where('receiver_id', $user->id);
+            })->orWhere(function($q) use ($user) {
+                $q->where('sender_id', $user->id)
+                  ->where('receiver_id', auth()->id());
+            })->first();
+
+            $user->friendship_status = $friendship ? $friendship->status : null;
+            $user->is_sender = $friendship && $friendship->sender_id == auth()->id();
+            
+            return $user;
+        });
     }
+
+    return view('search.index', compact('users', 'query'));
+}
 
 }
